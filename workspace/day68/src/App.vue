2e130f21a7bf3d262c7e119ce79810698893acc3
PANGJINI-D <geniedev97@gmail.com>
2024-09-02 21:53:50 +0900

    240902 라이프사이클, 템플릿, 디렉티브

---
+++ workspace/day68/src/App.vue
@@ -0,0 +1,56 @@
+  <!-- OptionsAPI -->
+  <!-- <template>
+    <div>
+      <button @click="increment">카운트:{{ counter }}</button>
+    </div>
+  </template>
+
+  <script>
+    export default {
+      data() {
+        return{
+          counter: 0,
+        };
+      },
+      methods: {
+        increment() {
+          this.counter++;
+        }
+      },
+      mounted() {
+        console.log("----- mount -----");
+      },
+    }
+  </script>
+
+  <style scoped>
+  </style> -->
+
+
+
+  <!-- Composition API -->
+  <template>
+    <div>
+      <button @click="increment">카운트 : {{ counter }}</button>
+    </div>
+  </template>
+
+  <script>
+    import { onMounted, ref } from 'vue';
+
+    export default {
+      setup(){
+        const counter = ref(0);
+
+        const increment = () => counter.value++;
+
+        onMounted(() => {
+          console.log("-----mount-----");
+        });
+
+        return {
+          counter, increment,
+        };
+      },
+    };
+</script>
